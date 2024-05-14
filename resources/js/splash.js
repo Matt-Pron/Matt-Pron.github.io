5d7ee485e917c5e8b90340d636875788058f707d
Matt-Pron <97278377+Matt-Pron@users.noreply.github.com>
2024-05-14 02:54:32 -0300

    capitulo iv y greeting

---
+++ resources/js/splash.js
@@ -0,0 +1,20 @@
+const list = [
+    "cucufato", 
+    "gazmoño", 
+    "beato", 
+    "santurrón",
+    "mojigato",
+    "buen hombre",
+    "puritano",
+    "meapilas",
+    "chupacirios",
+    "tragasantos",
+    "pechoño",
+    "bonachón",
+    "bienhechor",
+    "macanudo",
+];
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("greeting").innerHTML = list[Math.floor(Math.random() * list.length)]
+});
