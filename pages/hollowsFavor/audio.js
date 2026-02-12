1a78cbeac39a1bbc0ae3f78ea46be67228e4814e
Matt-Pron <matiaspronzati@gmail.com>
2026-02-12 20:32:31 -0300

    audio loop fix

--- pages/hollowsFavor/audio.js
+++ pages/hollowsFavor/audio.js
@@ -1,8 +1,39 @@
-const bgm = new Audio('./hollowsFavorTheme8.ogg');
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const gainNode = audioContext.createGain();
 
-bgm.loop = true;
-bgm.volume = 0.15;
+gainNode.gain.value = 0.15;
+gainNode.connect(audioContext.destination);
 
-export function startTheme() {
-	bgm.play()
+let themeBuffer;
+
+async function loadTheme() {
+	try {
+		const response = await fetch('./hollowsFavorTheme8.ogg');
+		const arrayBuffer = await response.arrayBuffer();
+		themeBuffer = await audioContext.decodeAudioData(arrayBuffer);
+	} catch (e) {}
 }
+
+export async function startTheme() {
+	if (audioContext.state === 'suspended') {
+		await audioContext.resume();
+	}
+
+	if (!themeBuffer) {
+		await loadTheme();
+	}
+
+	if (themeBuffer) {
+		const source = audioContext.createBufferSource();
+		source.buffer = themeBuffer;
+
+		source.loop = true;
+
+		source.connect(gainNode);
+		source.start(0);
+	}
+
+}
+
+loadTheme();
+
