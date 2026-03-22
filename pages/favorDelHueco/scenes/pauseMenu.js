f6282f51bb2a54da008ed74556217c62013dbc61
Matt Pron <matiaspronzati@gmail.com>
2026-03-22 10:41:30 -0300

    stats

--- pages/favorDelHueco/scenes/pauseMenu.js
+++ pages/favorDelHueco/scenes/pauseMenu.js
@@ -77,8 +77,9 @@ export class PauseMenu extends MenuViewport {
             });
 
             viewportManager.popUI();
-            this.customBtn.setContent(this.customBtn.content = 'PERSONALIZAR'
-                ? 'BLOQUEAR UI' : 'PERSONALIZAR');
+            this.customBtn.label = this.customBtn.label = 'PERSONALIZAR'
+                ? 'BLOQUEAR UI' : 'PERSONALIZAR';
+            this.computeLayout();
         }
         if (input.action ===  "exit") {
             viewportManager.setScene(MainMenu);
