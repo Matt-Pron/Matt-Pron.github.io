841be619a09c278eb6fef8c6d17e2773d6edb6a0
Matt Pron <matiaspronzati@gmail.com>
2026-03-22 22:02:31 -0300

    virtual keyboard

--- pages/favorDelHueco/scenes/touchpad.js
+++ pages/favorDelHueco/scenes/touchpad.js
@@ -20,7 +20,7 @@ class Touchpad extends Viewport {
             .setAlignment(CENTER, BOTTOM)
             .add(
                 new UIElement('Virtual keyboard')
-                    .setSize(Globals.cols, FIT)
+                    .setSize(GROW, FIT)
                     .setFlow(HORIZONTAL)
                     .setAlignment(CENTER, CENTER)
                     .add(
@@ -30,7 +30,19 @@ class Touchpad extends Viewport {
                             .setColor(4)
                     )
                     .add(
+                        new UIButton('Target', 'Q', 'target')
+                            .setSize(3, 3)
+                            .setBackground(5)
+                            .setColor(4)
+                    )
+                    .add(
                         new UIButton('Confirm', 'E', 'confirm')
+                            .setSize(3, 3)
+                            .setBackground(5)
+                            .setColor(4)
+                    )
+                    .add(
+                        new UIButton('Extra', 'R', 'extra')
                             .setSize(3, 3)
                             .setBackground(5)
                             .setColor(4)
@@ -53,6 +65,8 @@ class Touchpad extends Viewport {
     processInput(pointer) {
         if (!this.active) return false;
 
+        let currentEmulatedKey = null;
+
         if (pointer.isDown) {
             const cellW = pointer.canvasW / Globals.cols;
             const cellH = pointer.canvasH / Globals.rows;
@@ -62,14 +76,29 @@ class Touchpad extends Viewport {
             const pxW = this.pad.computedW * cellW;
             const pxH = this.pad.computedH * cellH;
 
-            const hit = this.pad.getHit(pointer.canvasX, pointer.canvasY, pxX, pxY, pxW, pxH);
+            const padHit = this.pad.getHit(pointer.canvasX, pointer.canvasY, pxX, pxY, pxW, pxH);
 
-            if (hit && hit.emulatedKey) {
-                eventBus.emit("touchpad_isDown");
-                if (this.activeEmulatedKey && this.activeEmulatedKey !== hit.emulatedKey) {
-                    Input.handleKey({ key: this.activeEmulatedKey }, false);
+            if (padHit && padHit.emulatedKey) {
+                currentEmulatedKey = padHit.emulatedKey;
+            } else {
+                const uiHit = this.getHit(pointer.x, pointer.y);
+
+                if (uiHit && uiHit.action) {
+                    const actionToKey = {
+                        'menu': 'escape',
+                    };
+
+                    if (actionToKey[uiHit.action]) {
+                        currentEmulatedKey = actionToKey[uiHit.action];
+                    }
                 }
-                this.activeEmulatedKey = hit.emulatedKey;
+            }
+        }
+
+        if (currentEmulatedKey) {
+            eventBus.emit("touchpad_isDown");
+
+            if (this.activeEmulatedKey && this.activeEmulatedKey !== currentEmulatedKey) {
                 Input.handleKey({ key: this.activeEmulatedKey }, true);
 
                 return true;
@@ -77,17 +106,33 @@ class Touchpad extends Viewport {
                 Input.handleKey({ key: this.activeEmulatedKey }, false);
                 this.activeEmulatedKey = null;
             }
-        } else if (this.activeEmulatedKey) {
-            Input.handleKey({ key: this.activeEmulatedKey }, false);
-            this.activeEmulatedKey = null;
-        }
 
-        return null;
+            return false;
+        }
+        //         eventBus.emit("touchpad_isDown");
+        //         if (this.activeEmulatedKey && this.activeEmulatedKey !== padHit.emulatedKey) {
+        //             Input.handleKey({ key: this.activeEmulatedKey }, false);
+        //         }
+        //         this.activeEmulatedKey = padHit.emulatedKey;
+        //         Input.handleKey({ key: this.activeEmulatedKey }, true);
+        //
+        //         return true;
+        //     } else if (this.activeEmulatedKey) {
+        //         Input.handleKey({ key: this.activeEmulatedKey }, false);
+        //         this.activeEmulatedKey = null;
+        //     }
+        // } else if (this.activeEmulatedKey) {
+        //     Input.handleKey({ key: this.activeEmulatedKey }, false);
+        //     this.activeEmulatedKey = null;
+        // }
+        //
+        // return null;
     }
 
     draw(renderer) {
         if (!this.active) return;
-        this.pad.draw(renderer);
+        super.draw(renderer);
+        // this.pad.draw(renderer);
     }
 }
 
