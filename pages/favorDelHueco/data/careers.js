932369e3f16f56206f0f9caa6993866a90151cce
Matt Pron <matiaspronzati@gmail.com>
2026-03-28 20:12:18 -0300

    v1

--- pages/favorDelHueco/data/careers.js
+++ pages/favorDelHueco/data/careers.js
@@ -14,7 +14,7 @@ export const CAREERS = {
             ItemData.weapons.honda,
             { type: 'ammo', id: 'piedras', amount: 8 }
         ],
-        exits: ['Fanático', 'Miliciano'],
+        exits: ['Chasqui'],
     },
     Forajido: {
         name: 'Forajido',
@@ -25,19 +25,20 @@ export const CAREERS = {
             ItemData.armor.armadura_acolchada,
             { type: 'ammo', id: 'piedras', amount: 8 }
         ],
-        exits: ['Cazador', 'Salteador de Caminos'],
+        // exits: ['Cazador', 'Salteador de Caminos'],
+        exits: ['Chasqui'],
     },
     Estudiante: {
         name: 'Estudiante',
-        advances: { WS: 1, BS: 0, S: 1, T: 1, Ag: 0, Int: 0, A: 0, W: 2, M: 0, Mag: 0 },
+        advances: { WS: 1, BS: 1, S: 0, T: 0, Ag: 0, Int: 2, A: 0, W: 1, M: 0, Mag: 0 },
         gear: [],
-        exits: ['Fanático', 'Miliciano'],
+        exits: ['Chasqui'],
     },
     Monje: {
         name: 'Monje',
-        advances: { WS: 1, BS: 0, S: 1, T: 1, Ag: 0, Int: 0, A: 0, W: 2, M: 0, Mag: 0 },
+        advances: { WS: 1, BS: 0, S: 1, T: 1, Ag: 0, Int: 1, A: 0, W: 1, M: 0, Mag: 0 },
         gear: [],
-        exits: ['Fanático', 'Miliciano'],
+        exits: ['Chasqui'],
     },
     Escudero: {
         name: 'Escudero',
@@ -48,7 +49,7 @@ export const CAREERS = {
             ItemData.armor.armadura_acolchada,
             { type: 'ammo', id: 'jabalinas', amount: 2 }
         ],
-        exits: ['Fanático', 'Miliciano'],
+        exits: ['Chasqui'],
     },
     Vagabundo: {
         name: 'Vagabundo',
@@ -57,27 +58,33 @@ export const CAREERS = {
             ItemData.weapons.daga,
             { type: 'ammo', id: 'piedras', amount: 8 }
         ],
-        exits: ['Fanático', 'Miliciano'],
+        exits: ['Chasqui'],
     },
     Pescador: {
         name: 'Pescador',
-        advances: { WS: 1, BS: 0, S: 1, T: 1, Ag: 0, Int: 0, A: 0, W: 2, M: 0, Mag: 0 },
+        advances: { WS: 0, BS: 0, S: 1, T: 2, Ag: 0, Int: 0, A: 0, W: 2, M: 0, Mag: 0 },
         gear: [
             ItemData.weapons.daga,
             ItemData.weapons.honda,
             { type: 'ammo', id: 'piedras', amount: 8 }
         ],
-        exits: ['Fanático', 'Miliciano'],
+        // exits: ['Fanático', 'Miliciano'],
+        exits: ['Chasqui'],
     },
     Herbolario: {
         name: 'Herbolario',
-        advances: { WS: 1, BS: 0, S: 1, T: 1, Ag: 0, Int: 0, A: 0, W: 2, M: 0, Mag: 0 },
+        advances: { WS: 0, BS: 0, S: 1, T: 1, Ag: 0, Int: 1, A: 0, W: 2, M: 0, Mag: 0 },
         gear: [
             ItemData.weapons.daga,
             ItemData.weapons.honda,
             { type: 'ammo', id: 'piedras', amount: 8 }
         ],
-        exits: ['Fanático', 'Miliciano'],
+        exits: ['Chasqui'],
+    },
+    Chasqui: {
+        name: 'Chasqui',
+        advances: { WS: 10, BS: 10, S: 10, T: 10, Ag: 10, Int: 10, A: 4, W: 20, M: 4, Mag: 4 },
+        exits: [],
     },
 };
 
