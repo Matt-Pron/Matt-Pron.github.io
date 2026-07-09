c3056b11b44f1ebced8bb5eb44bec26ba0c9f1a7
Matt-Pron <97278377+Matt-Pron@users.noreply.github.com>
2026-07-09 17:25:01 -0300

    Add files via upload

---
+++ pages/quests/quests.js
@@ -0,0 +1,48 @@
+export const questDatabase = {
+    "gaskell": {
+        "npcName": "El Comisario Gaskell (y el Sargento Maddox)",
+        "quests": [
+            {
+                "id": "g1",
+                "title": "Recuperar los libros perdidos",
+                "desc": "No, no soy Sir Phillip Sidney, soy el Comisario Gaskell y él es el Sargento Maddox.<br><br>Revisando la mercadería de contrabando decomisada, noté que alguien había robado algunos tomos.<br><br>Necesito que recuperes estos libros para mí antes de que comience el proceso judicial. Y date prisa, si caen en manos equivocadas, su contenido puede alterar el orden público.",
+                "objectives": [
+                    { "text": "Unfinished Tales of Númenor and Middle-Earth - JRR Tolkien - Editorial William Morrow - Inglés, Tapa Dura", "current": 1, "required": 1, "url": "https://a.co/d/05vfCRWf" },
+                    { "text": "The Fall of Gondolin - JRR Tolkien - Editorial William Morrow - Inglés, Tapa Dura", "current": 1, "required": 1, "url": "https://a.co/d/06fK8FVH" },
+                    { "text": "Beren and Lúthien - JRR Tolkien - Editorial William Morrow - Inglés, Tapa Dura", "current": 0, "required": 1, "url": "https://a.co/d/058HJCZr" },
+                    { "text": "The Fall of Númenor - JRR Tolkien - Editorial William Morrow - Inglés, Tapa Dura", "current": 1, "required": 1, "url": "https://a.co/d/02bc4Ezr" },
+                    { "text": "The Complete History of Middle-Earth - JRR Tolkien - Editorial William Morrow - Inglés, Tapa Dura", "current": 0, "required": 1, "url": "https://a.co/d/0a86cLmJ" },
+                ]
+            }
+        ]
+    },
+    "braulio": {
+        "npcName": "Braulio, el manitas",
+        "quests": [
+            {
+                "id": "b1",
+                "title": "Problema de plagas",
+                "desc": "Las ratas están devorando la cosecha. A éste ritmo no quedará comida para pasar el invierno.<br><br>Si me traes estas cuatro cuerdas del tamaño exacto, podré fabricar un aparato que emita ondas que afecten a los roedores, aumentando su apetito.",
+                "objectives": [
+                    { "text": "Cuerda E para bajo", "current": 0, "required": 1, "url": "https://www.mercadolibre.com.ar/ernie-ball-2834-encordado-bajo-4-cuerdas-super-slinky-45-100/p/MLA21809480?pdp_filters=item_id%3AMLA2470200876&matt_tool=89488245&ua=Fu-E7ENID1H-Vum8CZUmwBtuDcMH62_IhBxXtePB7xhgD3Y#origin=share&sid=share&wid=MLA2470200876&action=copy" },
+                    { "text": "Cuerda A para bajo", "current": 0, "required": 1, "url": "https://www.mercadolibre.com.ar/ernie-ball-2834-encordado-bajo-4-cuerdas-super-slinky-45-100/p/MLA21809480?pdp_filters=item_id%3AMLA2470200876&matt_tool=89488245&ua=Fu-E7ENID1H-Vum8CZUmwBtuDcMH62_IhBxXtePB7xhgD3Y#origin=share&sid=share&wid=MLA2470200876&action=copy" },
+                    { "text": "Cuerda D para bajo", "current": 0, "required": 1, "url": "https://www.mercadolibre.com.ar/ernie-ball-2834-encordado-bajo-4-cuerdas-super-slinky-45-100/p/MLA21809480?pdp_filters=item_id%3AMLA2470200876&matt_tool=89488245&ua=Fu-E7ENID1H-Vum8CZUmwBtuDcMH62_IhBxXtePB7xhgD3Y#origin=share&sid=share&wid=MLA2470200876&action=copy" },
+                    { "text": "Cuerda G para bajo", "current": 0, "required": 1, "url": "https://www.mercadolibre.com.ar/ernie-ball-2834-encordado-bajo-4-cuerdas-super-slinky-45-100/p/MLA21809480?pdp_filters=item_id%3AMLA2470200876&matt_tool=89488245&ua=Fu-E7ENID1H-Vum8CZUmwBtuDcMH62_IhBxXtePB7xhgD3Y#origin=share&sid=share&wid=MLA2470200876&action=copy" },
+                ]
+            }
+        ]
+    },
+    "casimiro": {
+        "npcName": "Casimiro, el girondino",
+        "quests": [
+            {
+                "id": "b1",
+                "title": "Ovejas voladoras",
+                "desc": "Llevo tres horas intentando enseñar a volar a mis ovejas, pero apenas si logran levitar unos pocos centímetros. Quizás con un buen café tengan la energía necesaria.<br><br>Dicen que los vietnamitas saben como prepararlo, voy a necesitar una de sus cafeteras... O dos, una por oveja.",
+                "objectives": [
+                    { "text": "Phin - De entre 100 y 200 ml - De ser posible x2", "current": 0, "required": 2, "url": "https://a.co/d/0evnQOb9" },
+                ]
+            }
+        ]
+    },
+};
